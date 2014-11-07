538630413b58b1b1e2e786176213ba087a5baa1d
Collin Meyers <cfmeyers@gmail.com>
2014-11-07 13:32:43 -0500

    add new search functionality, bootstrap styling

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
     };
     window.template = function(id){ return _.template( $('#' + id).html() );}
     window.urlBuilder = function(params){
-	var baseURL =  'http://cuny-first-papi.herokuapp.com/sections'
+	// var baseURL = 'http://localhost:3000/sections'
+	var baseURL = 'http://cuny-first-papi.herokuapp.com/sections'
 	
 	if ( _.isEmpty(params) ) {
 	    return baseURL+"?verbose=true";
@@ -27,14 +28,29 @@ $(document).ready(function(){
     
     App.Models.Section = Backbone.Model.extend();
     
+//COLLECTION    
     App.Collections.Sections = Backbone.Collection.extend({
-    	model: App.Models.Section
+    	model: App.Models.Section,
+	// initialize: function(){
+	    // this.on('remove', this.hideModel);
+	// },
+
+	hideModel: function(model){
+	    model.trigger('hide');
+	    console.log("hiding");
+	}
     });
 
-
+//ITEM VIEW
     App.Views.Section = Backbone.View.extend({
     	tagName: "li",
     	template: template('sectionTemplate'),
+	
+	initialize: function(){
+	    this.model.on('hide', function(){console.log(this.el);}, this);
+	    this.model.on('hide', this.remove, this);
+
+	},
 
     	render: function(){
     	    this.$el.html(this.template(this.model.toJSON()));
@@ -43,6 +59,7 @@ $(document).ready(function(){
     	}
     });
 
+//COLLECTION VIEW
     App.Views.SectionList = Backbone.View.extend({
     	tagName: "ul",
     	render: function(){
@@ -54,11 +71,39 @@ $(document).ready(function(){
 		
     	    }, this);
 
-    	}
+    	},
+	wipe: function(){
+	    console.log("The collection started with length "+this.collection.length)
+	    this.collection.each(function(model){
+		console.log("removing model "+ model.get("id"));
+		// this.collection.remove(model);
+		this.collection.hideModel(model);
+	    },this);
+	    this.collection.reset();
+	    console.log("wiped");
+	    console.log("The collection is now of length "+this.collection.length)
+
+	}
 
     });
 
+    var sectionCollection = new App.Collections.Sections();
+    var sectionListView = new App.Views.SectionList({collection: sectionCollection});
+    // window.sectionCollection = new App.Collections.Sections();
+    // window.sectionListView = new App.Views.SectionList({collection: sectionCollection});
+
     var params = {};
+
+//NEW SEARCH BUTTON    
+    $("#new_search").on("click", function(){
+	console.log("Begin wipe");
+	sectionCollection.each(function(model){console.log(model.get("id"));})
+	sectionListView.wipe();
+	$("#new_search").addClass("hidden");
+	$("#searchOptions").show();
+    });
+
+
     
     $("#department_id_select").on("change", function() {
 	params["department_id"] = $( "#department_id_select option:selected" ).val();
@@ -85,19 +130,21 @@ $(document).ready(function(){
     });
 
 
-
     $("#searchOptions").submit(function(event){
 	var url = urlBuilder(params);
 	console.log(url);
 
-	sectionCollection = new App.Collections.Sections();
 	sectionCollection.url = url;
 	sectionCollection.fetch();
-	sectionListView = new App.Views.SectionList({collection: sectionCollection});
 	
+	
+	$("#searchOptions").hide();
+	$("#wait").removeClass("hidden");
 	$.when( sectionCollection.fetch() ).done(function(){
-	    console.log(sectionCollection.length);
+	    console.log("sectionCollection is of length "+sectionCollection.length);
 	    sectionListView.render(); 
+	    $("#wait").addClass("hidden");
+	    $("#new_search").removeClass("hidden");
 	    $("#app").append(sectionListView.$el);
 	}).fail(function(){ alert("failed to load from API"); });
 
